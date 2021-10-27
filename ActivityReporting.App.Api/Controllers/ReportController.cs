178828352aa0396f18f1ed4cb5380416edbec775
Tyler-McIntyre <tmcintyre802@gmail.com>
2021-10-26 20:06:27 -0400

    add project files to repo

---
+++ ActivityReporting.App.Api/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using ActivityReporting.App.Api.Interfaces;
+using ActivityReporting.App.Api.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ActivityReporting.App.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        private readonly IDatabase iDatabase = new InMemDatabase();
+
+        [HttpPost]
+        [Route("/activity/{key}")]
+        public async Task<ActionResult> Post([FromBody] ActivityLogReq activity, [FromRoute] string key)
+        {
+            activity.SetKey(key);
+
+            await Task
+                .Run(() => iDatabase
+                .Log(activity));
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("activity/{key}/total")]
+        public async Task<ActionResult<ActivityResp>> Get(string key)
+        {
+            return Ok(await Task
+                .Run(() => new ActivityResp(iDatabase
+                .Sum(key
+                .ToUpper()))));
+        }
+    }
+}
---
+++ ActivityReporting.App.Api/Model/Activity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActivityReporting.App.Api.Model
+{
+    public class ActivityLogReq
+    {
+        [DefaultValue("Generic Activity")]
+        [StringLength(maximumLength:50, ErrorMessage = "Please enter a key with 50 characters or less.", MinimumLength = 1)]
+        public string Key { get; private set; }
+
+        [Required]
+        [DefaultValue(1)]
+        [Range(1,100, ErrorMessage = "Please enter a number between 1 & 100")]
+        public decimal Value { get; set; }
+        public DateTime CreatedDateTime { get; private set; }
+
+        public ActivityLogReq()
+        {
+            CreatedDateTime = DateTime.Now;
+        }
+
+        public void SetKey(string key)
+        {
+            Key = key.ToUpper();
+        }
+    }
+
+    public class ActivityResp
+    {
+        public ulong Value { get; init; }
+
+        public ActivityResp(ulong value)
+        {
+            Value = value;
+        }
+    }
+}
